e6a111fe23a2691b288176b8c5b8bb1a0eb5701c
sergii.azizov <sergii.azizov@aperturegroup.com>
2016-10-13 17:40:05 +0300

    feature-1: initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import './assets/scss/main.scss';
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import Test from './components/test/index';
+
+render(<Test />, document.getElementById('app'));
