66de3583691226cab0786330a4789d0ffa684ba8
sergii.azizov <sergii.azizov@aperturegroup.com>
2016-10-13 18:08:54 +0300

    feature-1: fix eslint

---
+++ app/components/Counter/Counter.js
@@ -0,0 +1,13 @@
+import './Counter.scss';
+
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div className="fa fa-glass">5556</div>
+        );
+    }
+}
+
+export default Counter;
--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 import './assets/scss/main.scss';
 
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-import Test from './components/test/index';
+import Counter from './components/Counter';
 
-render(<Test />, document.getElementById('app'));
+render(
+    <Counter />,
+    document.getElementById('app')
+);
