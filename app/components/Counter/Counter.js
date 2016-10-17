763e07b9ae02625cc65aabec9853cedba4812570
sergii.azizov <sergii.azizov@aperturegroup.com>
2016-10-17 23:58:49 +0300

    feature-1: finish first 3 lesson

--- app/components/Counter/Counter.js
+++ app/components/Counter/Counter.js
@@ -1,11 +1,33 @@
 import './Counter.scss';
 
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import DevTool from 'mobx-react-devtools';
 
+// make observable array
+
+const appState = observable({
+    count: 0,
+});
+
+appState.increment = function () {
+    this.count++;
+};
+appState.decrement = function () {
+    this.count--;
+};
+
+@observer
 class Counter extends Component {
     render() {
         return (
-            <div className="fa fa-glass">5556</div>
+            <div>
+                <DevTool />
+                <div className="counter">{ appState.count }</div>
+                <button onClick={ appState.increment.bind(appState) }>+</button>
+                <button onClick={ appState.decrement.bind(appState) }>-</button>
+            </div>
         );
     }
 }
